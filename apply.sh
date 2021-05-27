cb38c5e99af7bc45509183d4aea4edfdc461d7ac
druskus20 <43417195+druskus20@users.noreply.github.com>
2021-05-27 03:43:07 +0200

    Fixed apply.sh bug, added jsonnet submodule

--- apply.sh
+++ apply.sh
@@ -8,11 +8,11 @@ printf "{"
 for source_path in $(find dotfiles -type f | grep -v ".gitignore"); do
   
   program_name="$(echo "$source_path" | sed 's|^/\?[^/]*/\([^/]*\)/.*$|\1|')"
-  source_path="$(echo "$source_path" | sed 's|^/\?[^/]*/[^/]*/||')"
+#########  source_path="$(echo "$source_path" | sed 's|^/\?[^/]*/[^/]*/||')"
   dest_path="$dotfiles_root/$source_path"
   
   if [ -z "$last_program_name" ]; then
-    printf "\t%s: {\n" "$program_name"
+    printf "\t%s: {\n" "'$program_name'"
   elif [ "$program_name" != "$last_program_name" ]; then
     printf "\t},\n"
     printf "\t%s: {\n" "$program_name"
