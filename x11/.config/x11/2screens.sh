3e3814db75a4e9e04146f0f21fbebae989637a74
druskus20 <druskus20@gmail.com>
2021-09-22 00:10:06 +0200

    Multi monitor changes

---
+++ x11/.config/x11/2screens.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x364 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate right --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off
