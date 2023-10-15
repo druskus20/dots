bd7ad1dde6dded59f56a37d02121e7ccf9b4bf7d
druskus20 <43417195+druskus20@users.noreply.github.com>
2023-10-15 04:14:12 +0200

    feat(tmux): close original session when running "tm"

--- zsh/.config/zsh/modules/functions.zsh
+++ zsh/.config/zsh/modules/functions.zsh
@@ -3,17 +3,6 @@ function fzf-history() {
 }
 zle -N fzf-history fzf-history 
 
-# tmux main
-function tm() {
-  if [ -z "$TMUX" ]; then
-    # If run from outside tmux, (create?) and attach to main
-    tmux attach-session -t main || tmux new-session -s main \; run-shell /home/drusk/.config/tmux/plugins/tmux-resurrect/scripts/restore.sh
-  else 
-    # If run from inside tmux, (create?) and switch to main
-    tmux switch-client -t main || (tmux new-session -s main -d \; run-shell /home/drusk/.config/tmux/plugins/tmux-resurrect/scripts/restore.sh) && tmux switch-client -t main
-  fi
-}
-zle -N tm tm
 
 
 # Kills all detached tmux sessions except 'main'
@@ -21,7 +10,7 @@ function tmux-kill-detached-sessions() {
   sessions=$(tmux list-sessions -F '#{session_attached} #{session_name}' | awk '/^0/{print $2}')
   for session (${(f)sessions}); do
     if [ "$session" != "main" ]; then
-      tmux kill-session -t "$session"
+      tmux kill-session -t "$session" 2>/dev/null
     fi
   done
 }
@@ -29,3 +18,15 @@ zle -N tmux-kill-detached-sessions tmux-kill-detached-sessions
 
 # Call it when the shell exits (also when you close the terminal)
 trap tmux-kill-detached-sessions EXIT
+
+# tmux main
+function tm() {
+  if [ -z "$TMUX" ]; then
+    # If run from outside tmux, (create?) and attach to main
+    tmux attach-session -t main || tmux new-session -s main \; run-shell /home/drusk/.config/tmux/plugins/tmux-resurrect/scripts/restore.sh
+  else 
+    # If run from inside tmux, (create?) and switch to main
+    tmux switch-client -t main || (tmux new-session -s main -d \; run-shell /home/drusk/.config/tmux/plugins/tmux-resurrect/scripts/restore.sh) && (tmux switch-client -t main && tmux-kill-detached-sessions)
+  fi
+}
+zle -N tm tm
