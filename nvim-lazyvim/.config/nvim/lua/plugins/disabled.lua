8e36ac438b1a41f0cc752625a6eb1b4966c60868
druskus20 <dev@druskus.com>
2025-04-12 18:17:02 +0200

    oil.nvim and flash

--- nvim-lazyvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim-lazyvim/.config/nvim/lua/plugins/disabled.lua
@@ -15,7 +15,7 @@ return {
   { "folke/tokyonight.nvim",               enabled = false },
   --{ "nvimdev/dashboard-nvim",              enabled = false },
   { "lucas-reineke/indent-blankline.nvim", enabled = false },
-  { "folke/flash.nvim",                    enabled = false },
+  { "folke/flash.nvim",                    enabled = true },
   { "folke/todo-comments.nvim",            enabled = false },
   { "nvim-neo-tree/neo-tree.nvim",         enabled = false },
   { "folke/persistence.nvim",              enabled = false },
