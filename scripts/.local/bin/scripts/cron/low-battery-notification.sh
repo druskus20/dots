2077f6952115c9a62bcb56295f819dbb8ec598eb
druskus20 <dev@druskus.com>
2025-06-29 16:53:29 +0200

    idk man fix the battery cron

--- scripts/.local/bin/scripts/cron/low-battery-notification.sh
+++ scripts/.local/bin/scripts/cron/low-battery-notification.sh
@@ -4,6 +4,19 @@
 # Notify me with notify-send if my battery is below 20%.
 # You can set this to run via cron.
 
-[ "$(cat /sys/class/power_supply/BAT0/status)" = "Charging" ] && exit
-[ "$(cat /sys/class/power_supply/BAT0/capacity)" -lt 20 ] &&
-notify-send -u critical "Battery critically low."
+BATTERY_PATH="/sys/class/power_supply/BAT0"
+THRESHOLD=20
+
+[ "$(cat $BATTERY_PATH/status)" = "Charging" ] && exit 0
+
+BATTERY_LEVEL=$(cat /sys/class/power_supply/BAT0/capacity)
+
+if [ "$BATTERY_LEVEL" -le $THRESHOLD ]; then
+
+    # Find user session bus address - this helps specially in cron jobs
+  export DISPLAY=:0
+  export XDG_RUNTIME_DIR="/run/user/$(id -u)"
+  export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
+
+  notify-send -u critical "Battery critically low: $BATTERY_LEVEL%"
+fi
